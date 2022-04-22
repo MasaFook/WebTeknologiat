071c902d9282516727b5e7d97ad88a195769e375
Matti Koskinen <Matti.Koskinen@student.lab.fi>
2022-04-22 17:37:25 +0300

    pt1

---
+++ JavaScriptTehtävät/scripts/imagegallery.js
@@ -0,0 +1,11 @@
+
+
+function showGrid(){
+     document.getElementById("img-box").classList.remove("column-container");
+     document.getElementById("img-box").classList.add("grid-container");
+ }
+ 
+ function showColumn(){
+     document.getElementById("img-box").classList.remove("grid-container");
+     document.getElementById("img-box").classList.add("column-container");
+ }
